898612864643f5ae189d04e037afe4f871d13884
Erika Nunes <nunes.erika92@gmail.com>
2022-04-08 21:03:44 -0300

    Organização do diretório

---
+++ src/assets/js/pagecursos.js
@@ -0,0 +1,24 @@
+function carousel() {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        stagePadding: 20,
+        margin: 10,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            550: {
+                items: 2,
+            },
+            790: {
+                items: 3,
+            },
+            1190: {
+                items: 4,
+            },
+
+        }
+    })
+}
+
