38174266e60dfa205adc8a7f16d5a56bda7c380f
Jonathan Simões <jonathan.simoes@etec.sp.gov.br>
2022-04-20 18:18:54 -0300

    Adicionando js para a função typeWriter

---
+++ src/assets/js/typeWriter.js
@@ -0,0 +1,15 @@
+var i = 0;
+var tag = document.getElementById("text");
+var html = document.getElementById("text").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 50;
+
+function typeWriter() {
+if (i <= txt.length) {
+    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+        i++;
+        setTimeout(typeWriter, speed);
+}}
+
+typeWriter();
