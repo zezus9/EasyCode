2e0d217c73a8cdf9dc2a66e908068114b4dcec23
Jonathan Simões <jonathan.simoes@etec.sp.gov.br>
2022-05-13 22:35:33 -0300

    Adicionando Pesquisa na Página de Curso

--- src/php/pesquisa-pagecursos.php
+++ src/php/pesquisa-pagecursos.php
@@ -7,39 +7,38 @@
     $query = mysqli_query($sql, "SELECT * FROM curso WHERE linguagem LIKE '%$busca%'");
     $num   = mysqli_num_rows($query);
 
-    
-    if($num >0){
+    if($num > 0){
         echo "
             <div class='row'>
                 <div>
                     <h3>Pesquisa Rápida</h3>
-                    <div class='owl-carousel'>
-        "; 
-        while($card = mysqli_fetch_array($query)){
+                    <div class='cardPesquisa'>
+        ";
+        while($card = mysqli_fetch_assoc($query)){
             echo 
             "
-            <div class='card'>
-                <figure class='fundocard'>
-                    <img src='../assets/img/logo_cursos/$card[logo]'>
-                </figure>
-                <div class='abadedescricao'>
-                    <div class='abacard'>
-                        <div>
-                            <h5 class='titulo'>$card[linguagem]</h5>
-                            <span class='subtitulo'>
-                                <i class='fa-solid fa-clock'></i>
-                                Duração: $card[duracao]
-                            </span>
-                            <br>
-                            <a href='template_cursos.php' class='btn botao'>
-                                <i class='bi bi-pencil-square'>&nbsp;</i>
-                                Inscrever-se
-                            </a>
+                <div class='cardBusca'>
+                    <figure class='fundocard'>
+                        <img src='../assets/img/logo_cursos/$card[logo]'>
+                    </figure>
+                    <div class='abadedescricao'>
+                        <div class='abacard'>
+                            <div>
+                                <h5 class='titulo'>$card[linguagem]</h5>
+                                <span class='subtitulo'>
+                                    <i class='fa-solid fa-clock'></i>
+                                    Duração: $card[duracao]
+                                </span>
+                                <br>
+                                <a href='template_cursos.php' class='btn botao'>
+                                    <i class='bi bi-pencil-square'>&nbsp;</i>
+                                    Inscrever-se
+                                </a>
+                            </div>
                         </div>
+                        <p class='descricao'>$card[desc_breve]</p>
                     </div>
-                    <p class='descricao'>$card[desc_breve]</p>
                 </div>
-            </div>
             ";
         }
         echo 
