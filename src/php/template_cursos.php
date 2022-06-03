73fa1b806522a40379114e414ce14124293f81dc
Erika Nunes <nunes.erika92@gmail.com>
2022-06-03 20:33:31 -0300

    colocando o hamburguer pra funcionar

--- src/php/template_cursos.php
+++ src/php/template_cursos.php
@@ -55,11 +55,12 @@
         <header>
             <nav class='navbar navbar-expand-lg navbar-light fixed-top Background nonSelect'>
                 <div class='container'>
-                    <a class='navbar-brand Lovelofont' href='Home.html'><img width='35' src='../assets/img/logoEASYCODE.png'
+                    <a class='navbar-brand Lovelofont'><img width='35' src='../assets/img/logoEASYCODE.png'
                             alt='Logo EC'>
                         EASYCODE</a>
-                    <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'
-                        aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                        <button class='navbar-toggler' type='button' data-bs-toggle='collapse'
+                        data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent'
+                        aria-expanded='false' aria-label='Toggle navigation'>
                         <span class='navbar-toggler-icon'></span>
                     </button>
                     <div class='collapse navbar-collapse Josefinfont' id='navbarSupportedContent'>
