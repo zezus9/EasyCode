01cd90e9626beaaf6edb62343da23b6db1feaacb
Jonathan Simões <jonathan.simoes@etec.sp.gov.br>
2022-06-08 20:38:51 -0300

    Atualizando perfil

--- src/php/perfil.php
+++ src/php/perfil.php
@@ -7,10 +7,7 @@
     }
     
     // !Testa se esta logado ou não
-    $logado = false;
     if (isset($_SESSION['matricula'])) {
-        // !Testa se quem está logado é aluno ou professor
-        $logado = true;
         $matricula = $_SESSION['matricula'];
         $usuario = substr($matricula,0,1) == 0 ? 'aluno' : 'professor';
     }
@@ -88,12 +85,13 @@
         <link rel='stylesheet' href='../assets/css/perfil.css'>
         <link rel='stylesheet' href='../assets/css/style.css'>
         <link rel='stylesheet' href='../assets/css/inputs.css'>
+        <link rel='stylesheet' href='../assets/css/sidebar.css'>
 
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css'>
 
         <link rel='stylesheet' href='../assets/css/pagecursos.css'>
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'>        
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'>
     </head>
     ";
 
